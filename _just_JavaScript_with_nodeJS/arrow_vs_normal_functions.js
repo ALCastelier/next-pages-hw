6cf121361f61c50061057eaf96e67250c9de5fc6
Abass <amh@mahdavi.org>
2024-02-12 13:54:57 +0100

    _just_JavaScript folder added

---
+++ _just_JavaScript_with_nodeJS/arrow_vs_normal_functions.js
@@ -0,0 +1,16 @@
+// normal say hello
+
+function normalSayHello(name) {
+  return ("hello " + name );
+}
+
+
+const arrowSayHello = (name) => {
+    return ("hello " + name);
+}
+
+
+console.log (normalSayHello("John"));
+
+console.log (arrowSayHello("Alice"));
+
