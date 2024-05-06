84a3a2bf40e06831380eb9e97f4d15f477a1b1fc
ALCastelier <augustin.castelier@gmail.com>
2024-05-06 14:56:55 +0200

    202405062emeniveau

---
+++ components/NavBar.jsx
@@ -0,0 +1,15 @@
+import NavBarItem from "@/components/NavBarItem";
+
+const NavBar = () => {
+    return (
+        <>
+            <div className="navbar">
+                <NavBarItem />
+                <NavBarItem />
+                <NavBarItem />
+            </div>
+        </>
+    )
+};
+
+export default NavBar;
